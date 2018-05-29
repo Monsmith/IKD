8e47491a15cf741cc67c034e2277347bd80bf070
Vachiravit <vachiravit.va@gmail.com>
2018-05-29 23:20:21 +0700

    new heroku cleardb configuration

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,1 +1,16 @@
-<?php $con = mysqli_connect("localhost","root","0800542868","ikingdom"); ?>
+<?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+$server = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$db = substr($url["path"], 1);
+$db['default']['hostname'] =$cleardb_server;
+$db['default']['username'] =$cleardb_username;
+$db['default']['password'] =$cleardb_password;
+$db['default']['database'] =$cleardb_db;
+
+
+
+
+$con = mysqli_connect("127.0.0.1:50264","azure","6#vWHD_$","ikingdom"); ?>
