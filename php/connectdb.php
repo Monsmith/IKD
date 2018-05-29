2c3736541ba09bac5ef8adc28e0afdf20c57e9c7
Vachiravit <vachiravit.va@gmail.com>
2018-05-30 00:08:45 +0700

    fouth

--- php/connectdb.php
+++ php/connectdb.php
@@ -1,10 +1,11 @@
 <?php
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$server = $url["us-cdbr-iron-east-04.cleardb.net"];
+$username = $url["bb7e611f8b5400"];
+$password = $url["6dde10b4"];
+$db = substr($url["ikingdom"], 1);
+
 $db['default']['hostname'] =$cleardb_server;
 $db['default']['username'] =$cleardb_username;
 $db['default']['password'] =$cleardb_password;
@@ -13,4 +14,4 @@ $db['default']['database'] =$cleardb_db;
 
 
 
-$con = mysqli_connect("127.0.0.1:50264","azure","6#vWHD_$","ikingdom"); ?>
+$con = mysqli_connect("us-cdbr-iron-east-04.cleardb.net","bb7e611f8b5400","6dde10b4","ikingdom"); ?>
